769c5771bcc80b7d9890b73c109efb6c97f0102d
Mariana Raio <marianaraiooo@gmail.com>
2025-04-28 13:06:56 -0300

    Interface Principal

---
+++ projeto01_2025/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: '#eee', padding: '10px', marginTop: '40px' }}>
+        <p>© 2025 - Todos os direitos reservados.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ projeto01_2025/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header style={{ backgroundColor: '#282c34', padding: '20px', color: '#fff' }}>
+        <h1>Meu Projeto React</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
